924248e16ef99ee19adce27ac65496d649a437eb
Jonas Stankevicius <jonasstankevicius@gmail.com>
2020-02-13 20:35:51 +0200

    Setup image view ui

---
+++ ImageUtils.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Media;
+using System.Windows.Interop;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Drawing.Imaging;
+
+namespace MandelbrotSet
+{
+    class ImageUtils
+    {
+        public static Bitmap CreateBitmap(int width, int height, int[] rgb)
+        {
+            Bitmap bitmap = new Bitmap(width, height);
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    var color = System.Drawing.Color.FromArgb(rgb[0], rgb[1], rgb[2]);
+                    bitmap.SetPixel(i, j, color);
+                }
+            }
+
+            return bitmap;
+        }
+
+        public static Bitmap CreateBitmap(int[,,] values)
+        {
+            var height = values.GetLength(0);
+            var width = values.GetLength(1);            
+
+            Bitmap bitmap = new Bitmap(width, height);
+
+            for(int i = 0; i < width; i++)
+            {
+                for(int j = 0; j < height; j++)
+                {
+                    var color = System.Drawing.Color.FromArgb(values[j,i,0], values[j,i,1], values[j,i,2]);
+                    bitmap.SetPixel(i, j, color);
+                }
+            }
+
+            return bitmap;
+        }
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        public static ImageSource ImageSourceFromBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+
+        public static void SaveImage(Bitmap bitmap, string filePath = "test.png")
+        {
+            bitmap.Save(filePath, ImageFormat.Png);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MandelbrotSet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var arr = GerenateColorsArray((int)ImageControl.Width, (int)ImageControl.Height);
+
+            //var bitmap = ImageUtils.CreateBitmap((int)ImageControl.Width, (int)ImageControl.Height, new int[] { 255, 0, 0 });
+            var bitmap = ImageUtils.CreateBitmap(arr);
+            //ImageUtils.SaveImage(bitmap);
+
+            var imageSource = ImageUtils.ImageSourceFromBitmap(bitmap);
+
+            ImageControl.Source = imageSource;
+        }
+
+        public static int[,,] GerenateColorsArray(int width, int height)
+        {
+            var arr = new int[height, width, 3];
+
+            for(int i = 0; i < height; i++)
+            {
+                for(int j = 0; j < width; j++)
+                {
+                    arr[i, j, 0] = 255;
+                    arr[i, j, 1] = 0;
+                    arr[i, j, 2] = 0;
+
+                    if (i == j)
+                    {
+                        arr[i, j, 0] = 0;
+                    }
+                }
+            }
+
+            return arr;
+        }
+    }
+}
